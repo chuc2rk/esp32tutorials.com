3c12b19fd26bee3a685ac21ab066e2bd1de5da12
chuc2rk <chuc2rk@gmail.com>
2023-12-20 14:24:28 +0700

    Update main.c

--- LED_blink/main/main.c
+++ LED_blink/main/main.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-static TaskHandle_t receiver_handler = NULL;
-
-void sender(void* params)
-{
-    while (true)
-    {
-        xTaskNotifyGive(receiver_handler);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
-    
-}
-
-void receiver(void* params)
-{
-    while (true)
-    {
-        ulTaskNotifyTake(pdFALSE, portMAX_DELAY);
-        printf("received notification\n");
-    }
-    
-}
+#define LED_PIN 3
 
 void app_main(void)
 {
-    xTaskCreate(&receiver, "sender", 2048, NULL, 2, &receiver_handler);
-    xTaskCreate(&sender, "receiver", 2048, NULL, 2, NULL);    
+    gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN; GPIO_MODE_OUTPUT);
+
+    int toggle;
+    while (true)
+    {
+        toggle = !toggle;
+        gpio_set_level(LED_PIN, toggle);
+        vTaskDelay(1000 / portTICK_RATE_MS)
+    }
+    
 }
